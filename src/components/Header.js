1bcfd5d7b3f0ce02e3c5a5e510c59a0d6886e27c
ShivamVerma380 <71377475+ShivamVerma380@users.noreply.github.com>
2022-04-29 23:01:05 +0530

    Working on reactstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,22 +8,24 @@ const Header = () => {
     return(
         <div>
             <Container>
-                <Row>
-                    <Col md={4}>
-                        <img style={{textAlign:"left"}} src="/assets/images.jpg" width={100} height={50}/>
-                    </Col>
-                    <Col md={8}>
-                        <h1 style={{textAlign:"left",marginLeft:100 , fontFamily:"Tapestry"}}>MAHAVIR</h1>
-                    </Col>
+                <Row xs="6">
+                    <Col md={4}><img style={{textAlign:"left"}} src = {require ('../assets/images.jpg')}/></Col>
+                    <Col md={8}><h1 style={{textAlign:"left",marginLeft:100 , fontFamily:"Tapestry"}}>MAHAVIR</h1></Col>
                 </Row>
 
             </Container>
             
-            
+        </div>
            
             
-        </div>
+        /* </div>
+        <header>
+        <img style={{marginLeft:30, marginTop:30}} src = {require ('../assets/images.jpg')} width={75} height={75} />
+        <h3 style={{textAlign:"left",marginLeft:150 ,fontFamily:"Tapestry"}}>MAHAVIR</h3>
+        </header>
+     */
     );
+    
 }
 
 export default Header;
