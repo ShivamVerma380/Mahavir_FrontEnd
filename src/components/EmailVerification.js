c66b58a9e2bdce19ec26e1ce649e49bd36e1d960
ShivamVerma380 <71377475+ShivamVerma380@users.noreply.github.com>
2022-05-04 12:23:34 +0530

    Working on integration of email api

--- src/components/EmailVerification.js
+++ src/components/EmailVerification.js
@@ -3,11 +3,35 @@ import Header from "./Header";
 import { Button, Container, Form, FormGroup, Input, Label,Row,Col } from "reactstrap";
 // import { Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import {useState,useEffect} from "react";
+import {axios} from "axios";
+
 function EmailVerification(){  
     const navigate = useNavigate(); 
-const verifyOTP=()=>{
-    navigate('/otp');
-}
+    const verifyOTP=()=>{
+        navigate('/otp');
+    }
+
+    const[otp,SetOtp] = useState(null);
+    const axios = require('axios');
+    //const FormData = require('form-data');
+    const FormData = require('form-data');
+    async function makeGetRequest(){
+        //const form_data = FormData();
+        const form_data = new FormData();
+        form_data.append("email","shivamvermasv380@gmail.com");
+
+        let res = await axios.get('http://localhost:8080/verify-email',form_data);
+        
+        let data = res.data;
+
+        console.log(data);
+
+
+    }
+
+    makeGetRequest();
+
     return(
         <div>
             <Header/>
