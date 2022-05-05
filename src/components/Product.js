2ff0cc92452376d55f39cc8f4769f52fcaba9831
ShivamVerma380 <71377475+ShivamVerma380@users.noreply.github.com>
2022-05-05 18:50:20 +0530

    Added horizontal scroll buttons

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,6 +5,18 @@ import Arrow from 'react-arrows';
 import Carousel from 'react-bootstrap/Carousel';
 import { Button } from "bootstrap";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
 
 function Product({title,productList}){
     var cards=<div>
@@ -14,96 +26,143 @@ function Product({title,productList}){
 
 
     
-    const leftScroll=()=>{
-      console.log("Left scroll");
-      const conent = document.querySelector('#content');
-      conent.scrollLeft += 300;
-    }
+    // const leftScroll=()=>{
+    //   console.log("Left scroll");
+    //   const conent = document.querySelector('#content');
+    //   conent.scrollLeft += 300;
+    // }
 
 
-    const rightScroll=()=>{
-      console.log("Right scroll");
-      const conent = document.querySelector('#content');
-      conent.scrollLeft -= 300;
-    }
+    // const rightScroll=()=>{
+    //   console.log("Right scroll");
+    //   const conent = document.querySelector('#content');
+    //   conent.scrollLeft -= 300;
+    // }
     
-    return(
-        <div>
-            <h3 style={{textAlign:"left",margin:10 ,padding:5}}>{title}</h3>
-            <Row>
-              <Col md={1}>
-              <button style={{marginTop:175}} onClick={leftScroll}>
-                Left
-              </button>
-              </Col>
-              <Col md={10}>
-              <Container>
-              <ScrollMenu 
-                LeftArrow={LeftArrow}
-                RightArrow={RightArrow}
-              >
-                {
-                    cards = productList.map(index=>{
-                        return(
+    // return(
+    //     <div>
+    //         <h3 style={{textAlign:"left",margin:10 ,padding:5}}>{title}</h3>
+    //         <Row>
+    //           <Col md={1}>
+    //           <button style={{marginTop:175}} onClick={leftScroll}>
+    //             Left
+    //           </button>
+    //           </Col>
+    //           <Col md={10}>
+    //           <Container>
+    //           <ScrollMenu 
+    //             LeftArrow={LeftArrow}
+    //             RightArrow={RightArrow}
+    //           >
+    //             {
+    //                 cards = productList.map(index=>{
+    //                     return(
             
-                            <Card className="card" style={{height:350 ,margin:10, padding:2}}>
-                               <CardImg className="this.props.img"
-                                   src={index.src}
-                                 />
-                                 <CardBody>
-                                   <CardTitle className="this.props.h5">
-                                        <h5><b>{index.title}</b></h5>
-                                    </CardTitle>
-                                    <CardSubtitle>
-                                        <h6>Rs {index.price}</h6>
-                                    </CardSubtitle>
-                                    <CardText className="this.props.p">
-                                        <p>{index.description}</p>
-                                   </CardText>
+    //                         <Card className="card" style={{height:350 ,margin:10, padding:2}}>
+    //                            <CardImg className="this.props.img"
+    //                                src={index.src}
+    //                              />
+    //                              <CardBody>
+    //                                <CardTitle className="this.props.h5">
+    //                                     <h5><b>{index.title}</b></h5>
+    //                                 </CardTitle>
+    //                                 <CardSubtitle>
+    //                                     <h6>Rs {index.price}</h6>
+    //                                 </CardSubtitle>
+    //                                 <CardText className="this.props.p">
+    //                                     <p>{index.description}</p>
+    //                                </CardText>
                                     
-                                </CardBody>
+    //                             </CardBody>
 
-                            </Card>
-                        )
-                    })
-                 }
-                </ScrollMenu>
-                </Container>
-                </Col>
-              <Col md={1}>
-                <button style={{marginTop:175}} onClick={rightScroll}>
-                  Right
-                </button>
-              </Col>
-            </Row>
+    //                         </Card>
+    //                     )
+    //                 })
+    //              }
+    //             </ScrollMenu>
+    //             </Container>
+    //             </Col>
+    //           <Col md={1}>
+    //             <button style={{marginTop:175}} onClick={rightScroll}>
+    //               Right
+    //             </button>
+    //           </Col>
+    //         </Row>
             
-        </div>
+    //     </div>
 
+    // );
+
+    return(
+      <div>
+        <h3 style={{textAlign:"left",margin:10 ,padding:5}}>{title}</h3>
+      <>
+      <Swiper
+        slidesPerView={6}
+        spaceBetween={5}
+        slidesPerGroup={3}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {
+          cards = productList.map(index=>{
+            return(
+              <SwiperSlide>
+              <Card className="card" style={{height:350, margin:10, padding:2}}>
+                <CardImg className="this.props.img"
+                  src={index.src}/>
+                  <CardBody>
+                    <CardTitle className="this.props.h5">
+                      <h5><b>{index.title}</b></h5>
+                    </CardTitle>
+                    <CardSubtitle>
+                    <h6>Rs {index.price}</h6>
+                    </CardSubtitle>
+                    <CardText className="this.props.p">
+                     <p>{index.description}</p>
+                  </CardText>
+                  </CardBody>
+              </Card>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </>
+    </div>
     );
 }
 
 
 
-function LeftArrow() {
-    const { isFirstItemVisible, scrollPrev } =
-      React.useContext(VisibilityContext);
+// function LeftArrow() {
+//     const { isFirstItemVisible, scrollPrev } =
+//       React.useContext(VisibilityContext);
   
-    return (
-      <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
-        Left
-      </Arrow>
-    );
-  }
+//     return (
+//       <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+//         Left
+//       </Arrow>
+//     );
+//   }
   
-  function RightArrow() {
-    const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
+//   function RightArrow() {
+//     const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
   
-    return (
-      <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
-        Right
-      </Arrow>
-    );
-  }
+//     return (
+//       <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
+//         Right
+//       </Arrow>
+//     );
+//   }
   
+
+
 
 export default Product;
