8a3e4477407b339259f42fa7097c8afe1a84f8aa
Shraddha Mulay <shraddhamulay09@gmail.com>
2022-05-27 00:44:43 +0530

    Cart Page

--- src/components/Shopping-Cart/Cart.js
+++ src/components/Shopping-Cart/Cart.js
@@ -1,45 +1,114 @@
 import React from 'react';
+import Header from "../Header";
 import { Card, CardHeader, CardText, CardBody,Row,
-  CardTitle, CardFooter, Button, Col } from 'reactstrap';
-
+  CardTitle, CardFooter, Button, Col,Container ,Table} from 'reactstrap';
+  import {FormControl,Form} from 'react-bootstrap';
   import { QuantityPicker } from 'react-qty-picker';
 
 function Cart() {
     
     return (
-        <div class="container">
-            <Card>
-    <Row>
-        <Col>
-        <CardBody>
-       
-        <img style={{height:"150px", width:"100%"}} src = {require ('https://www.nokia.com/shop/sites/default/files/styles/aspect_16_9_1200px/public/2020-12/2880X1620px-1_0.jpg?itok=dtn_TZqA')}/>
-        <br></br>  <br></br>  <br></br>  <br></br>
-        <hr></hr>
-  <Button  outline>Proceed to Buy</Button>
-       
-        </CardBody>
-        </Col>
-        <Col>
-        <CardBody>
-        <CardTitle>LG 1 Ton 5 Star Split Inverter Air Conditioner (PSQ13ENZE)</CardTitle>
-  <CardText>₹ 37,480 ₹ 61,990 40% off</CardText>
-  <CardTitle><i  class="fa fa fa-trash-o" style={{paddingLeft:"20px" , paddingRight:"10px"}}>Remove</i></CardTitle>
-        <center><QuantityPicker smooth/>
-
-        <br></br>
-        <CardText>FREE DELIVERY<br></br>Delivery in 1-3 Days</CardText></center>
-        <br></br>  <br></br>
         
-        </CardBody>
-        </Col>
         
-    </Row>
-    
-    </Card>
+      
+        <div >   
+           <Header/> 
+           <div className="Cart">
+           <Row>
+  
+            <Col sm={8}>
 
-    </div>
+            <Table>
+            <thead>
+                <tr>
+                <th> Order Summary (1 Item)</th>
+                <th><Form className="d-flex">
+                            <FormControl
+                            type="search"
+                            placeholder="Pincode Search"
+                        
+                            aria-label="Search"
+                            />
+                            
+                        </Form></th>
+                </tr>
+            </thead>
 
+            </Table>
+            <Table>
+            <tbody>
+                <tr>
+                <td><img  style={{height:"150px", width:"300px"}} src = {require ('../../assets/logo.jpg')}/>
+                </td>
+                
+                <td><QuantityPicker smooth/></td>
+                <td><table>
+                    <tr>
+                        <td>FREE DELIVERY</td>
+                        </tr>
+                        <tr>
+                        <td>Delivery in 1-3 Days</td>
+                        </tr>
+                        <tr>
+                        <td>( T&C apply)</td>
+                        </tr>
+                    </table>
+                </td>
+                </tr>
+                <tr>
+                <td><table>
+                    <tr>
+                        <td>LG 1 Ton 5 Star Split Inverter Air Conditioner</td>
+                        </tr>
+                        <tr>
+                        <td>(PSQ13ENZE)</td>
+                        </tr>
+                        <tr>
+                        <td>₹ 37,480 ₹ 61,990 40% off</td>
+                        </tr>
+                    </table>
+                </td>
+                <td></td>
+                <td><Button style={{height:"100%", width:"200px"}} variant="info">Proceed to Buy</Button></td>
+                </tr>
+                </tbody>
+            </Table>
+            
+
+                </Col>
+            <Col sm={4}>
+            <Table style={{margin_top:"50px", color:'black'}} >
+            <thead>
+                <tr>
+                <th> Price Details</th>
+                <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>Price (1 Item)</td>
+                <td>₹ 37,480</td>
+                
+                </tr>
+                <tr>
+                <td>Delivery Charges</td>
+                <td>Free</td>
+                </tr>
+                <tr>
+                <td> Total Amount</td>
+                <td>₹ 37,480</td>
+                </tr>
+                
+                
+            </tbody>
+            </Table>
+            </Col>
+            </Row>
+           </div>
+  
+  
+
+    </div>    
 
 
 
