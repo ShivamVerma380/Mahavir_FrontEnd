e5b650f7a899b394876ed52a8d77f984453c27d6
= <shivamvermasv380@gmail.com>
2022-06-13 22:34:10 +0530

    Working on dropdowns in add to compare

--- src/components/ProductsComparison/ComparisonHeader.js
+++ src/components/ProductsComparison/ComparisonHeader.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Row,Col ,NavItem ,NavDropdown,Form} from "react-bootstrap";
 
 
@@ -16,10 +17,53 @@ function ComparisonHeader({product}){
         arr.push("God");
     }
     
+    const [Brands,SetBrands] = useState([]);
+    const [isBrandsFetched,SetIsBrandsFetched] = useState(false);
+
+    const [brand,SetBrand] = useState("Choose Brand...")
+
+
+    const [Models,SetModels] = useState();
+    const [isBrandSelected,SetIsBrandSelected] = useState(false);
+    
+    const [model,SetModel] = useState("Choose Model....")
+    
+
+    useEffect(()=>{
+        if(length<4 && !isBrandsFetched){
+            axios.get("http://localhost:8080/get-add-to-compare-subcat/"+localStorage.getItem("Category")+"/Brand")
+                .then(function(response){
+                    if(response.status==200){
+                        console.log("Add To Compare SubCat",response.data);
+                        SetBrands(response.data);
+                    }
+                    SetIsBrandsFetched(true);
+                }).catch(function(error){
+                    console.log("error");
+                })
+
+        }
+    })
    
         
     const handleFormCheck=event=>{
         alert(event.target.value)
+    }
+
+    function handleBrandClick(brandName){
+        Brands.map(brand=>{
+            if(brand.subSubCategoryName===brandName){
+                SetBrand(brandName)
+                SetModel("Choose Model..")
+                SetModels(brand.modelResponses);
+                SetIsBrandSelected(true);
+            }
+        })
+        console.log("BrandSelected",brandName);
+    }
+
+    function handleModelClick(modelName){
+        SetModel(modelName);
     }
 
     console.log("length",length);
@@ -57,12 +101,48 @@ function ComparisonHeader({product}){
                             <br></br>
                             <h6 style={{ marginTop: "20px" }} >Add a Product</h6>
                             <br></br>
-                            <NavDropdown title="Choose Brand" id="">
+                            {
+                                (isBrandsFetched)?(
+                                    <div>
+                                    <NavDropdown title = {brand} id="brand">
+                                        {
+                                            Brands.map(index=>{
+                                                return(
+                                                    <NavItem onClick={()=>handleBrandClick(index.subSubCategoryName)}>{index.subSubCategoryName}</NavItem>
+                                                );
+                                            })
+                                        }
+                                    </NavDropdown>
+                                    </div>
+                                ):(
+                                    null
+                                )
+                            }
+                            {
+
+                                (isBrandSelected)?(
+                                    <div>
+                                    <NavDropdown title={model} id = "Models">
+                                        {   
+                                            Models.map(model=>{
+                                                return(
+                                                    <NavItem onClick={()=>handleModelClick(model.modelName)}>{model.modelName}</NavItem>
+                                                )
+                                            })
+                                        }
+                                    </NavDropdown>
+                                    </div>
+                                ):(
+                                    null
+                                )
+                            }
+                            
+{/*                             
                                 <NavItem>Iphone 12</NavItem>
                             
-                            </NavDropdown>
+                            </NavDropdown> */}
                             
-                            <NavDropdown title="Choose Product"></NavDropdown>
+                            {/* <NavDropdown title="Choose Product"></NavDropdown> */}
                         </Col>
                     );
                 })
