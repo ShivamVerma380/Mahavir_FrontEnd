fe7b1645c13f2e52c7c37dae990adcc24417039c
= <shivamvermasv380@gmail.com>
2022-06-13 23:56:15 +0530

    Integrated dropdown of add to compare

--- src/components/ProductsComparison/AddToCompareProducts.js
+++ src/components/ProductsComparison/AddToCompareProducts.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Header from "../Header";
-import { Button, Container, Row ,Col} from "react-bootstrap";
+import { Button, Container, Row ,Col,NavItem ,NavDropdown,Form} from "react-bootstrap";
 import ComparisonHeader from "./ComparisonHeader";
 import ComparisonHighlights from "./ComparisonHighlights";
 import ComparisonVariants from "./ComparisonVariants";
@@ -11,6 +11,19 @@ function AddToCompareProducts(){
 
     const [product,SetProduct] = useState([]);
     const [isProductFetched,SetIsProductFetched] = useState(false);
+
+    const[length,SetLength] = useState(); 
+
+    const [Brands,SetBrands] = useState([]);
+    const [isBrandsFetched,SetIsBrandsFetched] = useState(false);
+
+    const [brand,SetBrand] = useState("Choose Brand...")
+
+
+    const [Models,SetModels] = useState();
+    const [isBrandSelected,SetIsBrandSelected] = useState(false);
+    
+    const [model,SetModel] = useState("Choose Model....")
 
 
     var keys=[];
@@ -25,7 +38,7 @@ function AddToCompareProducts(){
     }
 
     useEffect(()=>{
-        if(!isProductFetched){
+        if(!isProductFetched && !isBrandsFetched){
             var modelNumbers = localStorage.getItem("CompareModels").split(',');
             console.log("Model Numbers",modelNumbers);
             var urls = [];
@@ -40,13 +53,78 @@ function AddToCompareProducts(){
                         console.log("response",response);
                         product.push(response.data);
                     })
+                    SetLength(product.length);
                     SetIsProductFetched(true);
+                    
                 })
             )
+            axios.get("http://localhost:8080/get-add-to-compare-subcat/"+localStorage.getItem("Category")+"/Brand")
+                .then(function(response){
+                    if(response.status==200){
+                        console.log("Add To Compare SubCat",response.data);
+                        SetBrands(response.data);
+                        SetIsBrandsFetched(true);
+                        
+                    }
+                    
+                }).catch(function(error){
+                    console.log("error");
+                })
+                
         }
         
 
     })
+
+    
+    var arr=[];
+    if(length===1){
+        arr.push("God");
+        arr.push("God");
+        arr.push("God");
+    }else if(length===2){
+        arr.push("God");
+        arr.push("God");
+    }else if(length===3){
+        arr.push("God");
+    }
+
+    
+
+    const handleFormCheck=event=>{
+        alert(event.target.value)
+    }
+
+    function handleBrandClick(brandName){
+        Brands.map(brand=>{
+            if(brand.subSubCategoryName===brandName){
+                SetBrand(brandName)
+                SetModel("Choose Model..")
+                SetModels(brand.modelResponses);
+                SetIsBrandSelected(true);
+            }
+        })
+        console.log("BrandSelected",brandName);
+    }
+
+    function handleModelClick(modelName,modelNumber){
+
+        console.log("model number",modelNumber);
+        axios.get("http://localhost:8080/get-products/"+modelNumber)
+            .then(function(response){
+                if(response.status==200){
+                    // product.push(response.data);
+                    
+                    SetProduct([...product,response.data])
+                    SetModel(modelName);
+                    SetLength(length+1);
+                }
+            }).catch(function(error){
+                console.log(error);
+            })
+        
+        SetModel(modelName);
+    }
 
     return(
         <div>
@@ -55,7 +133,80 @@ function AddToCompareProducts(){
                 (isProductFetched)?(
                     <div>
                     
-                    <ComparisonHeader product={product}/>
+                    <Row style={{marginTop:15}}>
+                    <Col md={1}>  
+                    </Col>
+                    <Col md={2}>
+                        <h5>{product[0].productName} vs others</h5>
+                        <br></br>
+                        <Form>
+                            <Form.Check type="checkbox"   label = "Show Only Differences" onChange={handleFormCheck}/>
+                        </Form>
+                    </Col>
+                    {
+                        product.map(index=>{
+                            return(
+                                <Col md={2}>
+                                    <img style={{ width: "10rem", alignContent: "center" }}  src={'data:image/jpg;base64,' + index.productImage1.data}></img>
+                                    <br></br>
+                                    <h6 style={{ marginTop: "20px" }}>{index.productName}</h6>
+                                    <h6 style={{}}>₹{index.productPrice}</h6>
+                                </Col>
+                            );
+                        })
+                    }
+           
+            
+            {
+                (isProductFetched)?(
+                    arr.map(index=>{
+                    return(
+                        <Col md={2}>
+                        <img style={{ width: "10rem", alignContent: "center", visibility: "hidden" }}></img>
+                        <br></br>
+                        <h6 style={{ marginTop: "20px" }} >Add a Product</h6>
+                        <br></br>
+                        {
+                            (isBrandsFetched)?(
+                                <div>
+                                
+                                <NavDropdown title = {brand} id="brand">
+                                    {
+                                    Brands.map(index=>{
+                                        return(
+                                            <NavItem onClick={()=>handleBrandClick(index.subSubCategoryName)}>{index.subSubCategoryName}</NavItem>
+                                        );
+                                    })
+                                    }
+                                </NavDropdown>
+                                </div>
+                            ):(
+                                null
+                            )
+                            }
+                            {
+
+                                (isBrandSelected)?(
+                                    <div>
+                                    <NavDropdown title={model} id = "Models">
+                                        {   
+                                        Models.map(model=>{
+                                            return(
+                                                <NavItem onClick={()=>handleModelClick(model.modelName,model.modelNumber)}>{model.modelName}</NavItem>
+                                            )
+                                        })
+                                        }
+                                    </NavDropdown>
+                                    </div>
+                                ):(
+                                    null
+                                )
+                          }
+                                    </Col>
+                                );
+                            })):(null)
+                        }
+                        </Row>
                     <Container>
                     <hr></hr>
                     </Container>
