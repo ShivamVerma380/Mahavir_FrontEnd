0f3c55d5dcec8fcbc06bca47aea0d38c9c6fa324
= <shivamvermasv380@gmail.com>
2022-06-18 00:07:13 +0530

    Fetched subCategories

--- src/components/Admin/Test/AddSubCategories.js
+++ src/components/Admin/Test/AddSubCategories.js
@@ -1,19 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Header from "../../Header";
+import {Row,Col,Form} from "react-bootstrap";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { Container } from "@mui/system";
+const AddSubCategories=()=>{
+    
+    console.log("Category Selected",localStorage.getItem("CategorySelected"));
+    console.log("Model Nos",localStorage.getItem("ModelNos"))
 
-function AddSubCategories(){
-    // const Categories = route.params;
-    // console.log("Category Selected",localStorage.getItem("CategorySelected"));
-    // console.log("Model Nos",localStorage.getItem("ModelNos"))
-    // return(
-    //     <div>
-    //         <p>God</p>
-    //     </div>
-            
-    // )
+    const[Category,SetCategories] = useState([]);
+    const[isCategoriesFetched,SetIsCategoriesFetched] = useState(false);
+
+
+    const[SubCategories,SetSubCategories] = useState([]);
+
+    useEffect(()=>{
+        if(!isCategoriesFetched){
+            axios.get("http://localhost:8080/get-categories/admin")
+            .then(function(response){
+                if(response.status==200){
+                    SetCategories(response.data);
+                    response.data.map(index=>{
+                        if(index.categoryName===localStorage.getItem("CategorySelected")){
+                            SetSubCategories(index.subCategories)
+                        }
+                    })
+                    SetIsCategoriesFetched(true);
+                }
+            }).catch(function(error){
+                console.log("Error",error);
+            })
+        }       
+    })
+
+    const handleSubSubCatChange=(e)=>{
+        console.log("subsubCat",e.target.value);
+    }
+
     return(
-        <div>
-            <p>God</p>
-        </div>
+            
+            (isCategoriesFetched)?(
+                SubCategories.map(index=>{
+                    return(
+                        <div>
+                            <div>
+                            <Container>
+                            <h4>{index.subCategoryName}</h4>
+                            {
+                                index.subSubCategories.map(subSubCat=>{
+                                    return(
+                                            <div>
+                                            <input name="radioSubSubCat" type="radio" value={subSubCat} onChange={(e)=>handleSubSubCatChange(e)}/>{subSubCat}
+                                            <br></br>
+                                            </div>    
+                                    )
+                                })
+                            }
+                            </Container>
+                            </div>
+                        </div>
+                    );
+                })
+            ):(
+                null
+            )
+        
+       
     );
 }
 
