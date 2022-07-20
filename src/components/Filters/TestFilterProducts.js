8488d96b3742cce005520973bf44096d8bd1b4f9
Omkar Khare <khareomkar24@gmail.com>
2022-07-20 19:46:36 +0530

    Filter Products category added successfully

--- src/components/Filters/TestFilterProducts.js
+++ src/components/Filters/TestFilterProducts.js
@@ -26,6 +26,9 @@ function TestFilterProducts(){
     const [isAddCompareClicked, setisAddCompareClicked] = useState(false);
     const [change, setChange] = useState(0);
 
+    const [categories,SetCategories] = useState([]);
+    const [isCategoriesFetched,SetIsCategoriesFetched] = useState(false); 
+
 
     //For  filter criterias
     const [filters, SetFilters] = useState();
@@ -33,21 +36,69 @@ function TestFilterProducts(){
 
     const [keySet, setKeyState] = useState([]); //To Store filters name
 
+    const[filterselected,SetFilterSelected] = useState([])
+    
+    if(localStorage.getItem("SubCategory")!=null && localStorage.getItem("SubSubCategory")!=null){
+        filterselected.push(localStorage.getItem("SubCategory")+"-"+localStorage.getItem("SubSubCategory"))
+    }
+    
+
+
+    
+    
+    
+
     useEffect(()=>{
-        if(!isProductsFetched && !isSelectedProductsFetched){
-            axios.get("http://localhost:8080/get-products")
+        if(!isProductsFetched && !isSelectedProductsFetched && !isCategoriesFetched){
+            axios.get("http://localhost:8080/get-products-by-category/"+localStorage.getItem("Category"))
             .then(function(response){
                 SetProducts(response.data);
-                SetSelectedProducts(response.data);
+                if(localStorage.getItem("SubCategory")==null || localStorage.getItem("SubSubCategory")==null){
+                    SetSelectedProducts(response.data);
+                }else{
+                    response.data.map(product=>{
+                        if(product.filtercriterias[localStorage.getItem("SubCategory")]===localStorage.getItem("SubSubCategory")){
+                            selectedProducts.push(product);
+                        }                  
+                    })
+                }
+                
+                // SetSelectedProducts(response.data);
                 SetIsSelectedProductsFetched(true);
                 SetIsProductsFetched(true);
             }).catch(function(error){
                 console.log("error",error);
             })
+
+            axios.get("http://localhost:8080/get-categories")
+                .then(function(response){
+                    response.data.map(cat=>{
+                        categories.push(cat.category);
+                    })
+                    SetIsCategoriesFetched(true);
+                }).catch(function(error){
+                    console.log("error",error);
+                })
             
+            
+            axios.get("http://localhost:8080/filtercriterias/"+localStorage.getItem("Category"))
+                .then(function(response){
+                    SetFilters(response.data.filterCriterias);
+                    for(var key in response.data.filterCriterias){
+                        keySet.push(key);
+                    }
+                    console.log("keySet",keySet)
+                    SetIsFiltersFetched(true)
+                    // SetFilters(response.data.filterCriterias)
+                    // SetIsFiltersFetched(true)
+                }).catch(function(error){
+                    console.log("error",error)
+                })
         }
         
     })
+
+    
 
     const addtocart=(model)=>{
         
@@ -131,10 +182,166 @@ function TestFilterProducts(){
     }
 
 
+    function handleCategoryCheck(cat){
+        var element = document.getElementById(cat)
+        if(element.checked ==true){
+            console.log(cat," is checked")
+            localStorage.setItem("Category",cat);
+            localStorage.removeItem("SubCategory")
+            localStorage.removeItem("SubSubCategory")
+            window.location.reload()
+        }else{
+            console.log(cat," is not checked")
+        }
+    }
+
+    const handleFormCheck=(index,f)=>{
+        console.log("index:",index,"    f:",f)
+
+        var element = document.getElementById(f);
+        if(element.checked){
+            var  arr= filterselected;
+            var flag = true;
+            arr.map((i,pos)=>{
+                var pair = i.split("-");
+                if(index===pair[0]){
+                    arr[pos]= index+"-"+pair[1]+";"+f;
+                    flag = false;
+                }
+            })
+            if(flag){
+                arr.push(index+"-"+f);
+            }
+            SetFilterSelected(arr);
+            var productsArray = [];
+            // console.log("products",products)
+            console.log("filterSelected",filterselected);
+            products.map(index=>{
+                var flag = true;
+                filterselected.map(a=>{
+                    var pair = a.split("-");
+                    // console.log("pair",pair)
+                    var key = pair[0];
+                    var values = pair[1].split(";");
+                    console.log("values",values)
+                    var valueflag= false;
+                    values.map(v=>{
+                        console.log(index.filtercriterias[key])
+                        if(index.filtercriterias[key].includes(v)){
+                            valueflag=true;  
+                        }
+                    })
+                    if(!valueflag){
+                        flag = false;
+                    }
+                })
+                if(flag){
+                    productsArray.push(index);
+                }
+            })
+            // console.log("Products Array",productsArray.length);
+            
+            SetSelectedProducts(productsArray);
+
+        }else{
+            var arr = filterselected;
+            arr.map((i,pos)=>{
+                var pair = i.split("-");
+                if(index===pair[0]){
+                    var values= pair[1].split(";");
+                    if(values.length==1){
+                        arr.splice(pos,1);
+                    }
+                    else{
+                        var str=index+"-";
+                        values.map(v=>{
+                            if(v!==f){
+                                str+=v+";";
+                            }
+                        })
+                        str= str.slice(0,str.length-1);
+                        arr[pos]=str;
+
+                    }
+                }
+            })
+            SetFilterSelected(arr);
+            var productsArray = [];
+            console.log("products",products)
+            console.log("filterSelected",filterselected);
+            products.map(index=>{
+                var flag = true;
+                filterselected.map(a=>{
+                    var pair = a.split("-");
+                    // console.log("pair",pair)
+                    var key = pair[0];
+                    var values = pair[1].split(";");
+                    // console.log("values",values)
+                    var valueflag= false;
+                    values.map(v=>{
+                        console.log(index.filtercriterias[key])
+                        if(index.filtercriterias[key]===v){
+                            valueflag=true;  
+                        }
+                    })
+                    if(!valueflag){
+                        flag = false;
+                    }
+                })
+                if(flag){
+                    productsArray.push(index);
+                }
+            })
+            console.log("Products Array",productsArray.length);
+            
+            SetSelectedProducts(productsArray);
+        }
+    }
+    
+
     return(
         <Row>
             <Col md={1}></Col>
-            <Col md={2}></Col>
+            <Col md={2}>
+                <h3>Category</h3>
+                {
+                    (isCategoriesFetched)?(
+                        categories.map(cat=>{
+                            return(
+                                <Form.Check type="radio" id={cat} value={cat}  label={cat} name="cat" defaultChecked={(cat===localStorage.getItem("Category"))?(true):(false)} onChange={()=>handleCategoryCheck(cat)}/>
+                            )
+                        })
+                    ):(
+                        null
+                    )
+                }
+                <br></br>
+                <h4>Filters</h4>
+                {
+                    (isFiltersFetched)?(
+                        keySet.map(index=>{
+                            return(
+                                <div>
+                                    <h6>{index}</h6>
+                                    {
+                                        filters[index].map(f=>{
+                                            return(
+                                                <Form>
+                                                    <Form.Check type="checkbox" id={f} value={f}  label={f}     defaultChecked={(f===localStorage.getItem("SubSubCategory") && index===localStorage.getItem("SubCategory"))?(true):(false)} onChange={()=>handleFormCheck(index,f)} />
+                                                </Form>
+                                            )
+                                        
+                                            
+                                        })
+                                    }
+                                </div>
+                            )
+                        })
+                    ):(
+                        null
+                    )
+                }
+            </Col>
             <Col>
             {
                     
