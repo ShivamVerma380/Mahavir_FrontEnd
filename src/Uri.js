e24127e964d5621c088115fab7d499ade101d595
Shivam Verma <shivamvermasv380@gmail.com>
2022-09-18 21:46:48 +0530

    Backend url changed
    Signed-off-by: Shivam Verma <shivamvermasv380@gmail.com>

--- src/Uri.js
+++ src/Uri.js
@@ -1,7 +1,7 @@
 
-var url = "http://localhost:8080";
+// var url = "http://localhost:8080";
 
-// var url = "http://mahavirelectronics.us-east-1.elasticbeanstalk.com";
+var url = "http://backend-env.eba-dim99idp.us-east-1.elasticbeanstalk.com";
 
 // var url = "https://brewingjava.co.in";
 
