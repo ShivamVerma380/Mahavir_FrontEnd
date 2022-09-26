8364185a8189ee703c060048779fd4928fdd5bc8
Shivam Verma <shivamvermasv380@gmail.com>
2022-09-26 16:51:06 +0530

    Added react lazy
    Signed-off-by: Shivam Verma <shivamvermasv380@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes,  } from 'react-router-dom';
 import Login from './components/Login-Signup/Login';
 import ProductDetails from './components/Items/ProductDetails';
 import Admin from './components/Admin/Admin';
@@ -13,10 +13,11 @@ import CategoryDropdowns from './components/Admin/CategoryDropdowns';
 import PendingDelivery from './components/Admin/Sidebar/PendingDelivery'
 import UserComplaint from './components/Items/UserComplaint'
 import AddressForm from './components/OrderSummary/AddressForm';
-import Payment from './components/Checkout/BuyNowCheckout/Payment';
+// import Payment from './components/Checkout/BuyNowCheckout/Payment';
 // import OrderSummary from './components/OrderSummary';
 import PaymentOption from './components/PaymentOption';
 import 'typeface-roboto';
+
 
 
 // import MyOrders from './components/Orders/MyOrders';
@@ -60,6 +61,12 @@ import PaymentReturns from './components/Footer/PaymentReturns';
 import TermsOfUse from './components/Footer/TermsOfUse';
 import PaymentSuccess from './components/Checkout/BuyNowCheckout/PaymentSuccess';
 
+
+// import App from './App';
+const App = React.lazy(()=>import('./App'));
+const Payment = React.lazy(() => import('./components/Checkout/BuyNowCheckout/Payment'));
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
@@ -70,7 +77,11 @@ root.render(
   
   <HashRouter>
     <Routes>
-      <Route path='/' element={<App/>}/>
+      <Route path='/' element={
+        <Suspense fallback={<div>Loading...</div>}>
+          <App/>
+        </Suspense>
+      }/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/productDetails' element = {<ProductDetails/>}/>
       <Route path='/admin' element = {<AdminLogin/>}/>
@@ -117,6 +128,14 @@ root.render(
       <Route path="/checkout" element={<AddressBuyNowSummary/>}/>
       <Route path='/summary' element={<BuyNowSummary/>}/>
       <Route path = "/payment" element = {<Payment/>}/>
+
+
+
+      <Route path = "/payment" element = {
+        <Suspense fallback={<div>Loading...</div>}>
+          <Payment/>
+        </Suspense>
+      }/>
 
       <Route path='/invoice' element={<Invoice/>}/>
       <Route path='/rate-products' element = {<Orders/>}/>
