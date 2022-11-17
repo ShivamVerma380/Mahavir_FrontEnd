e52d0f38265cf84e4ecd9f9284780718b8e6fa32
Shivam Verma <shivam.r.verma@seagate.com>
2022-11-17 18:09:06 +0530

    Delete build directory

--- build/static/js/699.c12bff37.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmahavir=self.webpackChunkmahavir||[]).push([[699],{5699:function(e,n,r){r.r(n);r(2791);var a=r(184);n.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h5",{children:"God"})})}}}]);
-//# sourceMappingURL=699.c12bff37.chunk.js.map
