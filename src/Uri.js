598bb9c06d8a1cf60403b9b8a0cf98779a8e7185
Shraddha Mulay <76682198+shraddhamulay09@users.noreply.github.com>
2023-01-28 15:57:14 +0530

    Text visibility in admin sidebar bug fixed

--- src/Uri.js
+++ src/Uri.js
@@ -1,7 +1,7 @@
-var url = "http://localhost:9292";
+// var url = "http://localhost:9292";
 
-var url = "http://localhost:9292";
+// var url = "http://localhost:9292";
 
-// var url = "https://mahavirelectronics.org";
+var url = "https://mahavirelectronics.org";
 
 export default url;
